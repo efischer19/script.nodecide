22a162ff637178feef9bbf5ca5aa657b98be90fe
efischer19 <efischer0119@gmail.com>
2016-10-22 18:03:26 -0400

    Here goes nothing

--- nodecide.py
+++ nodecide.py
@@ -7,6 +7,7 @@ import xbmc
 # Constants
 IDEAL_QUEUE_LENGTH = 30  # This works out to ~10 hours of tv if uninterrupted
 LAST_QUEUE_FILE = "resources/last_queue.json"
+INFILE = "resources/in.data"
 
 def execute_log_command(cmd):
     """
@@ -50,6 +51,20 @@ def current_playlist():
             "items": current_playlist_contents["result"]["items"]
         }
     return None
+
+def current_item():
+    """
+    Returns the id of the item that is currently playing.
+    """
+    current_item_cmd = {
+        "jsonrpc": "2.0",
+        "method": "Player.GetItem",
+        "id": "getItem",
+        "params": {
+            "playerid": active_video_player(),
+        }
+    }
+    return execute_log_command(current_item_cmd)["id"]
 
 def get_last_queued_playlist():
     """
@@ -129,31 +144,102 @@ def get_last_played(item):
 
 def seed_last_queued():
     """
-    Seed the queue of script-determined items to play.
+    Return a list of fie paths
     """
-    raise NotImplementedError()
+    ret = []
+    with open(INFILE, "r") as in_data:
+        for _ in range(IDEAL_QUEUE_LENGTH):
+            ret.append(in_data.readline())
+    return ret
 
-def play_and_queue(items, index):
+def play_and_queue(items, index, use_ids=True):
     """
     Plays items[index] from where it was last stopped, and queues items[index:].
     """
-    xbmc.log(
-        "in play_and_queue, items: {}, index: {}".format(
-            items, index
-        ), xbmc.LOGDEBUG
-    )
+    play_cmd = {
+        "jsonrpc": "2.0",
+        "method": "Player.Open",
+        "id": "openPlayer",
+        "params": {
+            "item": {
+                "episodeid" if use_ids else "path": items[index],
+            },
+            "options": {
+                "resume": "true"
+            }
+        }
+    }
+    execute_log_command(play_cmd)
+
+    playlistid = current_playlist()["id"]
+
+    batch_queue_cmd = [
+        {
+            "jsonrpc": "2.0",
+            "method": "Playlist.Add",
+            "id": "queueEp",
+            "params": {
+                "playlistid": playlistid,
+                "item": {
+                    "episodeid" if use_ids else "path": item,
+                }
+            }
+        } for item in items[index:]
+    ]
+    execute_log_command(batch_queue_cmd)
 
 def trim_active_playlist():
     """
     Removes all items in the currently active playlist that occur before the current item.
     """
-    pass  # (Playlist.Remove, using indices and playlistid)
+    playlist = current_playlist()
+    item = current_item()
+    bulk_remove_cmd = [
+        {
+            "jsonrpc": "2.0",
+            "method": "Playlist.Remove",
+            "id": "removeEp",
+            "params": {
+                "playlistid": playlist["id"],
+                "position": index
+            }
+        } for index in range([item["id"] for item in playlist["items"]].index(item))
+    ]
+    execute_log_command(bulk_remove_cmd)
 
 def pad_active_playlist():
     """
-    Randomly selects new items to fill out the current playlist.
+    Add items to fill out the current playlist.
     """
-    pass
+    playlist = current_playlist()
+    with open(INFILE, "r+") as infile:
+        new_paths = []
+        for _ in range(IDEAL_QUEUE_LENGTH - len(playlist["items"])):
+            new_paths.append(infile.readline())
+        infile.seek(0)
+        point = 0
+        for path in infile:
+            infile.seek(point)
+            if path in new_paths:
+                infile.write("\n")
+            else:
+                infile.write(path + "\n")
+            point = infile.tell()
+
+    batch_queue_cmd = [
+        {
+            "jsonrpc": "2.0",
+            "method": "Playlist.Add",
+            "id": "queueEp",
+            "params": {
+                "playlistid": playlist["id"],
+                "item": {
+                    "episodeid": item,
+                }
+            }
+        } for item in new_paths
+    ]
+    execute_log_command(batch_queue_cmd)
 
 def main():
     """
@@ -171,12 +257,10 @@ def main():
         goto_next(active_player)
     else:
         if len(last_queued) == 0:
-            last_queued = seed_last_queued()
-            latest_index = -1
+            play_and_queue(seed_last_queued(), 0, use_ids=False)
         else:
             latest_index = calc_most_recent(last_queued["items"])
-        play_and_queue(last_queued, latest_index)
-        currently_viewing = current_playlist()
+            play_and_queue(last_queued, latest_index)
 
     trim_active_playlist()
     pad_active_playlist()
---
+++ resources/seed.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+shuf master_ref.data -o in.data
